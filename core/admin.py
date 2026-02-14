7d533eaf9fafe4582f4637d57620322b476c8d02
yared <yaredberhanu246@gmail.com>
2026-02-13 23:32:50 -0500

    add admin component

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Book, Genre, BookGenres, UserBook, Scan, ScanDetectedBook
 
-# Register your models here.
+admin.site.register(Book)
+admin.site.register(Genre)
+admin.site.register(BookGenres)
+admin.site.register(UserBook)
+admin.site.register(Scan)
+admin.site.register(ScanDetectedBook)
